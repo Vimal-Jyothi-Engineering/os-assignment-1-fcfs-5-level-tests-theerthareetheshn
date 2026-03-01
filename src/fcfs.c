0567cb16ea8074eb7018d71664dc57dba93101d4
Theertha Reethesh Nambiar <theerthareetheshn@gmail.com>
2026-03-01 17:45:35 +0530

    Updated fcfs.c

--- src/fcfs.c
+++ src/fcfs.c
@@ -1,1 +1,67 @@
+#include <stdio.h>
 
+struct Process {
+    char pid[10];
+    int arrival;
+    int burst;
+    int waiting;
+    int turnaround;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Process p[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(p[j].arrival > p[j + 1].arrival) {
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+
+    float totalWT = 0, totalTAT = 0;
+    int currentTime = 0;
+
+    currentTime = p[0].arrival;
+    p[0].waiting = 0;
+    currentTime += p[0].burst;
+    p[0].turnaround = p[0].waiting + p[0].burst;
+
+    totalWT += p[0].waiting;
+    totalTAT += p[0].turnaround;
+
+    for(int i = 1; i < n; i++) {
+        p[i].waiting = currentTime - p[i].arrival;
+
+        if(p[i].waiting < 0)
+            p[i].waiting = 0;
+
+        currentTime += p[i].burst;
+        p[i].turnaround = p[i].waiting + p[i].burst;
+
+        totalWT += p[i].waiting;
+        totalTAT += p[i].turnaround;
+    }
+
+    printf("Waiting Time:\n");
+    for(int i = 0; i < n; i++)
+        printf("%s %d\n", p[i].pid, p[i].waiting);
+
+    printf("Turnaround Time:\n");
+    for(int i = 0; i < n; i++)
+        printf("%s %d\n", p[i].pid, p[i].turnaround);
+
+    printf("Average Waiting Time: %.2f\n", totalWT / n);
+    printf("Average Turnaround Time: %.2f\n", totalTAT / n);
+
+    return 0;
+}
