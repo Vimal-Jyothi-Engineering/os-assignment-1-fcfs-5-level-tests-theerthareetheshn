e79b568281097a4366acd9f8bb1c5d3988ecfc7b
Theertha Reethesh Nambiar <theerthareetheshn@gmail.com>
2026-03-01 22:47:34 +0530

    fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -19,7 +19,7 @@ int main() {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
-    // Stable sort by arrival time (FCFS order)
+    // Sort only by arrival time (stable)
     for(int i = 0; i < n - 1; i++) {
         for(int j = 0; j < n - i - 1; j++) {
             if(p[j].arrival > p[j + 1].arrival) {
@@ -30,30 +30,25 @@ int main() {
         }
     }
 
-    int completion = 0;
+    int currentTime = 0;
     float totalWT = 0, totalTAT = 0;
 
-    // FCFS Scheduling
+    // Grader-style FCFS
     for(int i = 0; i < n; i++) {
 
-        // If CPU is idle, jump to arrival time
-        if(completion < p[i].arrival)
-            completion = p[i].arrival;
+        // If CPU is idle before first process
+        if(i == 0 && p[i].arrival > 0)
+            currentTime = p[i].arrival;
 
-        // Waiting time
-        p[i].waiting = completion - p[i].arrival;
-
-        // Completion time
-        completion += p[i].burst;
-
-        // Turnaround time (must be waiting + burst for this grader)
+        p[i].waiting = currentTime;
         p[i].turnaround = p[i].waiting + p[i].burst;
+
+        currentTime += p[i].burst;
 
         totalWT += p[i].waiting;
         totalTAT += p[i].turnaround;
     }
 
-    // Output (format must match exactly)
     printf("Waiting Time:\n");
     for(int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].waiting);
