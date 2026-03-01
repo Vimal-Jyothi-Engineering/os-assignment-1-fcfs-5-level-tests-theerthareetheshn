51b5c0203dd9645b5f164146e3fd83c51433e763
Theertha Reethesh Nambiar <theerthareetheshn@gmail.com>
2026-03-01 16:55:08 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct Process {
     char pid[10];
+    int pid_num;      // numeric part of PID for sorting
     int arrival;
     int burst;
     int waiting;
@@ -18,12 +20,16 @@ int main() {
     // Read input
     for(int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
+        p[i].pid_num = atoi(p[i].pid + 1);  // extract number from "P1"
     }
 
-    // Stable Bubble Sort by arrival time (FCFS rule)
+    // Sort by arrival time, if tie sort by PID number
     for(int i = 0; i < n - 1; i++) {
         for(int j = 0; j < n - i - 1; j++) {
-            if(p[j].arrival > p[j+1].arrival) {
+            if (p[j].arrival > p[j+1].arrival ||
+               (p[j].arrival == p[j+1].arrival &&
+                p[j].pid_num > p[j+1].pid_num)) {
+
                 struct Process temp = p[j];
                 p[j] = p[j+1];
                 p[j+1] = temp;
@@ -36,10 +42,8 @@ int main() {
 
     for(int i = 0; i < n; i++) {
 
-        // Handle CPU idle time
-        if(currentTime < p[i].arrival) {
+        if(currentTime < p[i].arrival)
             currentTime = p[i].arrival;
-        }
 
         p[i].waiting = currentTime - p[i].arrival;
         p[i].turnaround = p[i].waiting + p[i].burst;
@@ -50,17 +54,13 @@ int main() {
         totalTAT += p[i].turnaround;
     }
 
-    // EXACT OUTPUT FORMAT
-
     printf("Waiting Time:\n");
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].waiting);
-    }
 
     printf("Turnaround Time:\n");
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].turnaround);
-    }
 
     printf("Average Waiting Time: %.2f\n", totalWT / n);
     printf("Average Turnaround Time: %.2f", totalTAT / n);
