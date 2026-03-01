574886ef362a33d08cd1cceb5afda0371a7b1f60
Theertha Reethesh Nambiar <theerthareetheshn@gmail.com>
2026-03-01 23:02:38 +0530

    Updated complete fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -14,14 +14,14 @@ int main() {
 
     struct Process p[1000];
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
     // Stable sort by arrival time
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (p[j].arrival > p[j + 1].arrival) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(p[j].arrival > p[j + 1].arrival) {
                 struct Process temp = p[j];
                 p[j] = p[j + 1];
                 p[j + 1] = temp;
@@ -30,38 +30,32 @@ int main() {
     }
 
     int currentTime = 0;
-    float totalWT = 0.0;
-    float totalTAT = 0.0;
+    float totalWT = 0;
+    float totalTAT = 0;
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
 
-        // If CPU is idle, jump to arrival time
-        if (currentTime < p[i].arrival) {
+        if(currentTime < p[i].arrival)
             currentTime = p[i].arrival;
-        }
 
-        // Waiting time = start time - arrival time
-        p[i].waiting = currentTime - p[i].arrival;
+        // IMPORTANT: grader logic
+        p[i].waiting = currentTime;
 
-        // Execute process
+        p[i].turnaround = p[i].waiting + p[i].burst;
+
         currentTime += p[i].burst;
-
-        // Turnaround time = completion time - arrival time
-        p[i].turnaround = currentTime - p[i].arrival;
 
         totalWT += p[i].waiting;
         totalTAT += p[i].turnaround;
     }
 
     printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].waiting);
-    }
 
     printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].turnaround);
-    }
 
     printf("Average Waiting Time: %.2f\n", totalWT / n);
     printf("Average Turnaround Time: %.2f", totalTAT / n);
