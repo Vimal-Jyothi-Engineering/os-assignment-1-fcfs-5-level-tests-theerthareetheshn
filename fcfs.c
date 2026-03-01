4c123436e542276f5291b781bc068776a5b255d7
Theertha Reethesh Nambiar <theerthareetheshn@gmail.com>
2026-03-01 23:15:16 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,67 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct {
-    char id[10];
+struct Process {
+    char pid[10];
     int arrival;
     int burst;
     int waiting;
     int turnaround;
-} Process;
-
-// Comparator for sorting by arrival time
-int compare(const void *a, const void *b) {
-    Process *p1 = (Process *)a;
-    Process *p2 = (Process *)b;
-
-    if (p1->arrival == p2->arrival)
-        return 0;
-    return p1->arrival - p2->arrival;
-}
+};
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    Process p[n];
+    struct Process p[1000];
 
-    for (int i = 0; i < n; i++) {
-        scanf("%s %d %d", p[i].id, &p[i].arrival, &p[i].burst);
-        p[i].waiting = 0;
-        p[i].turnaround = 0;
+    for(int i = 0; i < n; i++) {
+        scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
-    // 🔥 SORT BY ARRIVAL TIME (THIS FIXES LEVEL 3 & 4)
-    qsort(p, n, sizeof(Process), compare);
+    // Stable sort by arrival time
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(p[j].arrival > p[j + 1].arrival) {
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
 
     int currentTime = 0;
     float totalWT = 0.0, totalTAT = 0.0;
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
 
-        // If CPU is idle
-        if (currentTime < p[i].arrival) {
+        if(currentTime < p[i].arrival) {
             currentTime = p[i].arrival;
         }
 
-        // Waiting time
         p[i].waiting = currentTime - p[i].arrival;
-
-        // Turnaround time
         p[i].turnaround = p[i].waiting + p[i].burst;
 
-        // Advance time
         currentTime += p[i].burst;
 
         totalWT += p[i].waiting;
         totalTAT += p[i].turnaround;
     }
 
-    // Output per process
-    for (int i = 0; i < n; i++) {
+    // Print in grader expected format
+    for(int i = 0; i < n; i++) {
         printf("%s %d %d %d %d\n",
-               p[i].id,
+               p[i].pid,
                p[i].arrival,
                p[i].burst,
                p[i].waiting,
