a42288b2b2c4189d7d1fcec825cd52019b158f78
Theertha Reethesh Nambiar <theerthareetheshn@gmail.com>
2026-03-01 17:50:36 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -14,13 +14,13 @@ int main() {
 
     struct Process p[n];
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - i - 1; j++) {
-            if(p[j].arrival > p[j + 1].arrival) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (p[j].arrival > p[j + 1].arrival) {
                 struct Process temp = p[j];
                 p[j] = p[j + 1];
                 p[j + 1] = temp;
@@ -31,33 +31,30 @@ int main() {
     float totalWT = 0, totalTAT = 0;
     int currentTime = 0;
 
-    currentTime = p[0].arrival;
-    p[0].waiting = 0;
-    currentTime += p[0].burst;
-    p[0].turnaround = p[0].waiting + p[0].burst;
+    for (int i = 0; i < n; i++) {
 
-    totalWT += p[0].waiting;
-    totalTAT += p[0].turnaround;
+        int startTime;
 
-    for(int i = 1; i < n; i++) {
-        p[i].waiting = currentTime - p[i].arrival;
+        if (currentTime < p[i].arrival)
+            startTime = p[i].arrival;
+        else
+            startTime = currentTime;
 
-        if(p[i].waiting < 0)
-            p[i].waiting = 0;
-
-        currentTime += p[i].burst;
+        p[i].waiting = startTime - p[i].arrival;
         p[i].turnaround = p[i].waiting + p[i].burst;
+
+        currentTime = startTime + p[i].burst;
 
         totalWT += p[i].waiting;
         totalTAT += p[i].turnaround;
     }
 
     printf("Waiting Time:\n");
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].waiting);
 
     printf("Turnaround Time:\n");
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].turnaround);
 
     printf("Average Waiting Time: %.2f\n", totalWT / n);
